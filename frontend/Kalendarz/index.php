f9863c4a0c3a84a625f63e5d2f1a5e44e9b7058d
Marcin Tubisz <mr.tubisz@gmail.com>
2018-01-13 20:00:03 +0100

    forms added

--- frontend/Kalendarz/index.php
+++ frontend/Kalendarz/index.php
@@ -20,10 +20,59 @@ if(!isset($_SESSION['loggedIn']))
             <div id ="menu"><h4>Hello Marcin</h4></br>
                 <button id="menuButton">Expand the menu</button>
                 <ul>
-                    <li><a href="#">ADD TASK</a></li>
-                    <li><a href="#">DELETE TASK</a></li>
-                    <li><a href="#">CHECK TASK LIST</a></li>
+                    <li><span id="addEvent" >ADD TASK</span></li>
+                    <li><span id="removeEvent" >DELETE TASK</span></li>
+                    <li><span >CHECK TASK LIST</span></li>
                 </ul>
+				<div>
+				
+				<form action="#" method="post" id="formMeeting">
+					<h4>Meeting</h4><button type="button" id="goToTask">Create task</button><div style="clear:both;"></div>
+					<p>Creator: 
+						<input type="text" name="creator"/>
+					</p>
+					<p>Name: 
+						<input type="text" name="name"/>
+					</p>
+					<p>Description: 
+						<input type="text" name="description"/>
+					</p>
+					<p>Date: 
+						<input type="date" name="date"/>
+					</p>
+					<p>Time: 
+						<input type="time" name="time"/>
+					</p>
+					<p>Place: 
+						<input type="text" name="place"/>
+					</p>
+						<input type="submit" value="Send" />
+						<button type="button" class="quitCreating">Quit</button><div style="clear:both;"></div>
+				</form>
+				<form action="#" method="post" id="formTask">
+					<h4>Task</h4><button type="button" id="goToMeeting">Create meeting</button><div style="clear:both;"></div>
+					<p>Name: 
+						<input type="text" name="name"/>
+					</p>
+					<p>Description: 
+						<input type="text" name="description"/>
+					</p>
+					<p>Deadline: 
+						<input type="date" name="date"/>
+					</p>
+						<input type="hidden" name="status" value="doing" />
+						<input type="submit" value="Send" />
+						<button type="button" class="quitCreating">Quit</button><div style="clear:both;"></div>
+				</form>
+				
+				<form action="#" method="post" id="deleteEvent">
+					<p>Event name: 
+						<input type="text" name="place"/>
+					</p>
+					<input type="submit" value="Delete" />
+					<button type="button" class="quitCreating">Quit</button><div style="clear:both;"></div>
+				</form>
+				</div>
             </div>
         <div id = "container">
 
