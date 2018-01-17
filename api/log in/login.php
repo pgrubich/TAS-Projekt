874d67eeac928cacbac7e6fcf169a512b294a00a
Mateusz Wiśniewski <wisniewski.m.1996@gmail.com>
2018-01-17 21:30:54 +0100

    changed login function

--- api/log in/login.php
+++ api/log in/login.php
@@ -7,55 +7,28 @@ if((!isset($_POST['login'])) || (!isset($_POST['password']))) {
   exit();
 }
 
-require_once "../config/connect.php";
-mysqli_report(MYSQLI_REPORT_STRICT);
+$conn = require_once "../config/database.php";
 
-try {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
 
-    $connection = new mysqli($host, $dbUser, $dbPassword, $dbName);
+    $query = $conn->prepare("SELECT* FROM users WHERE login= :lgin");
+    $query -> bindValue(':lgin', $login, PDO::PARAM_STR);
+    $query -> execute();
 
-    if ($connection->connect_errno != 0) {
+    $user = $query->fetch();
 
-        throw new Exception(mysqli_connect_errno());
-    }
-    else {
-        $login = $_POST['login'];
-        $password = $_POST['password'];
+        if ($user && $password == $user['password']) {
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['login'] = $user['login'];
 
-        $login = htmlentities($login, ENT_QUOTES, "UTF-8");
-        $password = htmlentities($password, ENT_QUOTES, "UTF-8");
-
-
-        if ($result = $connection->query(sprintf
-        ("SELECT* FROM users WHERE login='%s'",
-            mysqli_real_escape_string($connection, $login)))) {
-            $numOfUsers = $result->num_rows;
-
-            if ($numOfUsers > 0) {
-                $row = $result->fetch_assoc();
-
-                if ($password == $row['password']) {
-                    $_SESSION['loggedIn'] = true;
-                    $_SESSION['login'] = $row['login'];
-
-                    unset($_SESSION['error']);
-                    $result->close();
-                    header('Location: ../../Kalendarz/index.php');
-                } else {
-                    $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło</span>';
-                    header('Location: ../index.php');
-                }
-            } else {
-                $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło</span>';
-                header('Location: ../index.php');
-            }
+            unset($_SESSION['error']);
+            header('Location: ../../Kalendarz/index.php');
+        } else {
+            $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło</span>';
+            header('Location: ../index.php');
         }
 
-        $connection->close();
-    }
-}
-catch(Exception $e) {
 
-    echo 'Błąd serwera <br>';
-}
+
 
